5f12c840c208869256d132c4168c455fab95c768
Cole <pickeric@uci.edu>
2023-01-13 14:25:08 -0800

    Testing with VS

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -4,6 +4,6 @@
 
 bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
 {
-    return true; // FYI, this stub is not a correct solution.
+    return false; // FYI, this stub is not a correct solution.
 }
 
