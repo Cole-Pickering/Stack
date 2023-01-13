02264ef06f4fb7a12857f6e9ce581b08c1ae162e
Cole <pickeric@uci.edu>
2023-01-13 15:40:02 -0800

    testing stoi

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -4,19 +4,19 @@
 
 bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
 {
-    std::string x1 = "";
+    std::string x1 = "1";
     for (int i = 0; i< s1.size(); i++){
-        x1 += mapping.at(s1[i]);
+        //x1 += mapping.at(s1[i]);
     } 
     
-    std::string x2 = "";
+    std::string x2 = "2";
     for (int i = 0; i< s2.size(); i++){
-        x2 += mapping.at(s2[i]);
+        //x2 += mapping.at(s2[i]);
     }
     
-    std::string x3 = "";
+    std::string x3 = "3";
     for (int i = 0; i< s3.size(); i++){
-        x3 += mapping.at(s3[i]);
+        //x3 += mapping.at(s3[i]);
     }
     if (std::stoi(x1) + std::stoi(x2) == std::stoi(x3)){
         return true;
