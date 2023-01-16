7f0a7b4347d6ab00c57efb184572d85cc2170538
Cole <pickeric@uci.edu>
2023-01-15 17:02:59 -0800

    Changed mapping responces to strings before concatination

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -1,22 +1,23 @@
 #include <unordered_map>
 #include <string>
 #include "verifySol.hpp"
+#include <math.h>
 
 bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
 {
-    std::string x1 = "1";
+    std::string x1 = 0;
     for (int i = 0; i< s1.size(); i++){
-        //x1 += mapping.at(s1[i]);
+        x1 += std::to_string(mapping.at(s1[i]));
     } 
     
-    std::string x2 = "2";
+    std::string x2 = 0;
     for (int i = 0; i< s2.size(); i++){
-        //x2 += mapping.at(s2[i]);
+        x2 += std::to_string(mapping.at(s2[i]));
     }
     
     std::string x3 = "3";
     for (int i = 0; i< s3.size(); i++){
-        //x3 += mapping.at(s3[i]);
+        x3 += std::to_string(mapping.at(s3[i]));
     }
     if (std::stoi(x1) + std::stoi(x2) == std::stoi(x3)){
         return true;
