34c312ff6192153df4c591b3b113ede1b5190f2d
Cole <pickeric@uci.edu>
2023-01-19 21:34:22 -0800

    removed unneccesary import

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -1,21 +1,20 @@
 #include <unordered_map>
 #include <string>
 #include "verifySol.hpp"
-#include <math.h>
 
 bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
 {
-    std::string x1 = 0;
+    std::string x1 = "";
     for (int i = 0; i< s1.size(); i++){
         x1 += std::to_string(mapping.at(s1[i]));
     } 
     
-    std::string x2 = 0;
+    std::string x2 = "";
     for (int i = 0; i< s2.size(); i++){
         x2 += std::to_string(mapping.at(s2[i]));
     }
     
-    std::string x3 = "3";
+    std::string x3 = "";
     for (int i = 0; i< s3.size(); i++){
         x3 += std::to_string(mapping.at(s3[i]));
     }
