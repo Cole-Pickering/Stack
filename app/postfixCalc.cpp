f2c9cae1aaca78d370c15984e1d65407ad323558
Cole <pickeric@uci.edu>
2023-01-19 17:07:56 -0800

    postfixCalc passes all test cases

--- app/postfixCalc.cpp
+++ app/postfixCalc.cpp
@@ -3,7 +3,58 @@
 
 unsigned postfixCalculator(const std::vector<std::string> & entries)
 {
-
-	return 1729; // stub, not the correct answer
+	int sec;
+	LLStack<int> calcStack;
+	for (int i =0; i<entries.size();i++){
+		if (entries[i] == "*"){
+			if (calcStack.size() < 2){
+				throw CannotEvaluateException("Stack Too Small");
+			}
+			sec = calcStack.top();
+			calcStack.pop();
+			sec = calcStack.top() * sec;
+			calcStack.pop();
+			calcStack.push(sec);
+		}
+		else if (entries[i] == "/"){
+			if (calcStack.size() < 2){
+				throw CannotEvaluateException("Stack Too Small");
+			}
+			sec = calcStack.top();
+			calcStack.pop();
+			sec = calcStack.top() / sec;
+			calcStack.pop();
+			calcStack.push(sec);
+		}
+		else if (entries[i] == "+"){
+			if (calcStack.size() < 2){
+				throw CannotEvaluateException("Stack Too Small");
+			}
+			sec = calcStack.top();
+			calcStack.pop();
+			sec = calcStack.top() + sec;
+			calcStack.pop();
+			calcStack.push(sec);
+		}
+		else if (entries[i] == "-"){
+			if (calcStack.size() < 2){
+				throw CannotEvaluateException("Stack Too Small");
+			}
+			sec = calcStack.top();
+			calcStack.pop();
+			sec = calcStack.top() - sec;
+			calcStack.pop();
+			calcStack.push(sec);
+		}
+		else{
+			calcStack.push(std::stoul(entries[i]));
+		}
+	}
+	if (calcStack.size() == 1){
+		return calcStack.top();
+	}
+	else{
+		throw CannotEvaluateException("Stack Too Small");
+	}
 }
 
